e2772d92898d63f0befa8c82a049a307b498b941
massiel-ch <massielchoqueluque@gmail.com>
2025-06-25 12:56:14 -0500

    Update campo.java

--- untitled/src/campo.java
+++ untitled/src/campo.java
@@ -1,2 +1,32 @@
 public class campo {
+    private String[][] campo;
+
+    public CampoDeBatalla(int filas, int columnas) {
+        campo = new String[filas][columnas];
+        inicializarCampo();
+    }
+
+    private void inicializarCampo() {
+        for (int i = 0; i < campo.length; i++) {
+            for (int j = 0; j < campo[i].length; j++) {
+                campo[i][j] = "."; 
+            }
+        }
+    }
+
+    public void mostrarCampo() {
+        for (int i = 0; i < campo.length; i++) {
+            for (int j = 0; j < campo[i].length; j++) {
+                System.out.print(campo[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public void colocarEnemigo(int x, int y) {
+        if (x >= 0 && x < campo.length && y >= 0 && y < campo[0].length) {
+            campo[x][y] = "E"; 
+        }
+    }
 }
+
