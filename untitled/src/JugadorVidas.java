6544cc4f89f1c9aac4d7dcfbd7a51c7ab0360020
massiel-ch <massielchoqueluque@gmail.com>
2025-07-14 13:11:37 -0500

    Create JugadorVidas.java

---
+++ untitled/src/JugadorVidas.java
@@ -0,0 +1,42 @@
+public class Jugador Vidas {
+    private int x, y;
+    private int vidas = 3;
+    private boolean estaVivo = true;
+
+    public Jugador(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public void recibirDaño() {
+        if (vidas > 0) {
+            vidas--;
+            System.out.println("¡Daño recibido! Vidas restantes: " + vidas);
+            if (vidas == 0) {
+                estaVivo = false;
+                System.out.println("¡Game Over!");
+            }
+        }
+    }
+
+    public void dibujar(Graphics g) {
+        g.setColor(Color.RED);
+        g.fillRect(x, y, 40, 20); 
+
+        g.setFont(new Font("Arial", Font.BOLD, 18));
+        g.setColor(Color.RED);
+        g.drawString("Vidas: " + vidas, 10, 20);
+    }
+
+    public boolean estaVivo() {
+        return estaVivo;
+    }
+
+    public void mover(int dx, int dy) {
+        x += dx;
+        y += dy;
+    }
+
+    public int getX() { return x; }
+    public int getY() { return y; }
+}
